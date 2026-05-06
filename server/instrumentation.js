21633bad8740bc88b082490cf95319f8b0dc81bf
Dmytro Shvaika <shvajka@gmail.com>
2026-05-06 22:40:26 +0300

    feat: updated multiplayer libs

--- server/instrumentation.js
+++ server/instrumentation.js
@@ -1,5 +1,7 @@
 import { NodeSDK } from '@opentelemetry/sdk-node'
+import { hostname } from 'os'
 import { ParentBasedSampler } from '@opentelemetry/sdk-trace-base'
+import { resourceFromAttributes, detectResources } from '@opentelemetry/resources'
 import {
   SessionRecorderIdGenerator,
   SessionRecorderHttpTraceExporter,
@@ -8,23 +10,42 @@ import {
   SessionRecorderTraceIdRatioBasedSampler
 } from '@multiplayer-app/session-recorder-node'
 import {
-  getNodeAutoInstrumentations
+  getNodeAutoInstrumentations,
+  getResourceDetectors
 } from '@opentelemetry/auto-instrumentations-node'
+import * as SemanticAttributes from '@opentelemetry/semantic-conventions'
 import {
   MULTIPLAYER_SDK_API_KEY,
   ENVIRONMENT,
-  OTLP_SAMPLE_RATE
+  OTLP_SAMPLE_RATE,
+  SERVICE_NAME,
+  SERVICE_VERSION
 } from './config.js'
 
 /**
  * Initialize OpenTelemetry with Multiplayer exporters and auto-instrumentation.
  * This module MUST be imported before any other application code.
  */
+
+const resourceWithAttributes = resourceFromAttributes({
+  [SemanticAttributes.SEMRESATTRS_SERVICE_NAME]: SERVICE_NAME,
+  [SemanticAttributes.SEMRESATTRS_SERVICE_VERSION]: SERVICE_VERSION,
+  [SemanticAttributes.SEMRESATTRS_HOST_NAME]: hostname(),
+  [SemanticAttributes.SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]: ENVIRONMENT,
+  [SemanticAttributes.SEMRESATTRS_PROCESS_RUNTIME_VERSION]: process.version,
+  [SemanticAttributes.SEMRESATTRS_PROCESS_PID]: process.pid,
+})
+const detectedResources = detectResources({
+  detectors: getResourceDetectors(),
+})
+const resource = resourceWithAttributes.merge(detectedResources)
+
 const sdk = new NodeSDK({
   sampler: new ParentBasedSampler({
     root: new SessionRecorderTraceIdRatioBasedSampler(OTLP_SAMPLE_RATE),
   }),
   traceIdGenerator: new SessionRecorderIdGenerator(),
+  resource,
   instrumentations: [
     ...getNodeAutoInstrumentations({
       '@opentelemetry/instrumentation-http': {
@@ -48,16 +69,11 @@ const sdk = new NodeSDK({
     })
   ],
   traceExporter: new SessionRecorderHttpTraceExporter({
-    apiKey: MULTIPLAYER_SDK_API_KEY
+    apiKey: MULTIPLAYER_SDK_API_KEY,
   }),
   logRecordExporter: new SessionRecorderHttpLogsExporter({
-    apiKey: MULTIPLAYER_SDK_API_KEY
+    apiKey: MULTIPLAYER_SDK_API_KEY,
   }),
-  resourceAttributes: {
-    'service.name': 'multiplayer-cli-demo-app',
-    'service.version': '0.0.0',
-    environment: ENVIRONMENT
-  }
 })
 
 sdk.start()
